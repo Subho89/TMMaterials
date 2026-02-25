ea01f2c8c2133a93546318c8619d9eb354026075
Subhodeep Bhattacharjee <subhotalk@gmail.com>
2026-02-25 14:18:56 +0530

    Updated with Excel feature

--- TMMaterials/ViewModel/MaterialPropertyDataVM.cs
+++ TMMaterials/ViewModel/MaterialPropertyDataVM.cs
@@ -1,5 +1,7 @@
-﻿using MvvmHelpers;
+﻿using GalaSoft.MvvmLight.Command;
+using MvvmHelpers;
 using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows.Input;
 using TMMaterials.DAL.Model;
 using TMMaterials.Services.ViewModels;
@@ -12,9 +14,14 @@ namespace TMMaterials.ViewModel
     {
         public string FullMaterialName { get; set; } // Binds to TextBlock
         public string MaterialType { get; set; }     // Binds to TextBlock
-
+        public ICommand ExportCommand { get; }
         // Binds to DataGrid ItemsSource
         public ObservableCollection<MaterialPropertyItem> PropertyItems { get; set; } = new();
+
+        public MaterialPropertyDataVM()
+        {
+            ExportCommand = new RelayCommand(OnExport);
+        }
 
         public void LoadProperties(List<MaterialPropertyItem> properties, string selectedName)
         {
@@ -29,6 +36,32 @@ namespace TMMaterials.ViewModel
 
             // Notify the UI that the header text changed
             OnPropertyChanged(nameof(FullMaterialName));
+        }
+
+        private void OnExport()
+        {
+            var sfd = new Microsoft.Win32.SaveFileDialog
+            {
+                Filter = "CSV Files (*.csv)|*.csv",
+                FileName = $"{FullMaterialName}_Properties"
+            };
+
+            if (sfd.ShowDialog() == true)
+            {
+                var builder = new StringBuilder();
+                // Header
+                builder.AppendLine("Property,Value,Unit");
+
+                // Data Rows from the Grid
+                foreach (var item in PropertyItems)
+                {
+                    // Ensure commas in values don't break the CSV format
+                    builder.AppendLine($"{item.PropertyName},{item.PropertyValue},{item.Unit}");
+                }
+
+                System.IO.File.WriteAllText(sfd.FileName, builder.ToString());
+                System.Windows.MessageBox.Show("Data exported successfully!");
+            }
         }
     }
 }
